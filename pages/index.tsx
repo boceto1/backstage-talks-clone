68baebfb38ffe51dedf4b142c61302f8bbaf6139
boceto1 <janka.obando@outlook.es>
2023-06-05 16:46:30 -0500

    feat: set up a draft skeleton for the app.
    We set placeholder and mock data to draft the page and set the initial
    styles

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,9 +1,11 @@
+import CatalogItem from './components/CatalogItem';
 import Information from './components/Information';
 
 export default function Home() {
   return (
     <div>
      <Information />
+     <CatalogItem />
     </div>
   )
 }
