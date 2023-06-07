bb1659428d2da9e34d7dfdc16b043a197655180f
boceto1 <janka.obando@outlook.es>
2023-06-06 21:24:48 -0500

    feat: Parameterize CatalogItem
    Create interface to parameterize CatalogItem props and update code to
    work with these props values.

--- pages/components/CatalogItem/index.tsx
+++ pages/components/CatalogItem/index.tsx
@@ -1,10 +1,57 @@
 import { styled } from 'styled-components';
 import Image from 'next/image';
+import { useCallback } from 'react';
 
-const Wrapper = styled.div`
+export interface ICatalogItem {
+  id: string,
+  title: string,
+  image: string,
+  background: string,
+  onPreorder?: (id: string) => void,
+  onSelectOnStore?: (id: string) => void,
+}
+
+const CatalogItem: React.FC<ICatalogItem> = ({
+  id,
+  title,
+  image,
+  background,
+  onPreorder,
+  onSelectOnStore,
+}) => {
+
+  const onHandlePreorder = useCallback(() => {
+    onPreorder?.(id);
+  }, [id]);
+
+  const onSelectedOnStore = useCallback(() => {
+    onSelectOnStore?.(id);
+  }, [id]);
+
+  return (
+    <Wrapper id={id} background={background}>
+      <Image
+        src={image}
+        alt={`Cover of ${title}`}
+        width={420}
+        height={531}
+      />
+      <Title>{title}</Title>
+      <PreorderCTA onClick={onHandlePreorder}>PRE-ORDER HERE</PreorderCTA>
+      <SelectStoresText>
+        or in <CTALink onClick={onSelectedOnStore}>selected stores</CTALink>
+      </SelectStoresText>
+    </Wrapper>
+  );
+};
+
+export default CatalogItem;
+
+
+const Wrapper = styled.div<{ background?: string }>`
   width: 100vw;
   height: 100vh;
-  background: #ef5f8d;
+  background: ${({ background }) => background || "white"};
   display: flex;
   justify-content: center;
   align-items: center;
@@ -15,34 +62,14 @@ const Title = styled.h1`
   margin-bottom: 18px;
 `;
 
-const Link = styled.a`
+const CTALink = styled.span`
   color: white;
 `;
 
-const PreorderText = styled(Link)`
+const PreorderCTA = styled(CTALink)`
   margin-bottom: 18px;
 `;
 
 const SelectStoresText = styled.p`
   margin-bottom: 18px;
 `;
-
-const CatalogItem: React.FC = () => {
-  return (
-    <Wrapper>
-      <Image
-        src="/catalog/cover_issue_1.png"
-        alt="Cover of Issue 1"
-        width={420}
-        height={531}
-      />
-      <Title>Issue #7</Title>
-      <PreorderText>PRE-ORDER HERE</PreorderText>
-      <SelectStoresText>
-        or in <Link>selected stores</Link>
-      </SelectStoresText>
-    </Wrapper>
-  );
-};
-
-export default CatalogItem;
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,11 +1,27 @@
-import CatalogItem from './components/CatalogItem';
+import CatalogItem, { ICatalogItem } from './components/CatalogItem';
 import Information from './components/Information';
 
+// Temp: Delete this
+const mockCatalogItem: ICatalogItem = {
+  id: 'issue-1',
+  title: 'Issue 1',
+  background: '#EF5F8D',
+  image: '/catalog/cover_issue_1.png',
+};
+
 export default function Home() {
+  // Temp: Delete this
+  const { id, title, background, image } = mockCatalogItem;
+
   return (
     <div>
-     <Information />
-     <CatalogItem />
+      <Information />
+      <CatalogItem
+        id={id}
+        title={title}
+        background={background}
+        image={image}
+      />
     </div>
-  )
+  );
 }
