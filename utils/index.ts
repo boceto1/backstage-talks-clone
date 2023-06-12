c1c00e27b214e974e3558d6d4823a5fe9a6748a5
boceto1 <janka.obando@outlook.es>
2023-06-12 15:01:43 -0500

    fix: remove console.log

--- utils/index.ts
+++ utils/index.ts
@@ -9,9 +9,7 @@ export const getClientURL = () => {
 
 
 export const calculateContrastColor = (background: string) => {
-  console.log(background);
   const [r, g, b] = background.match(/\w\w/g).map(hex => parseInt(hex, 16));
-  console.log(r,g,b);
   // Calculate the relative luminance of the background color
   const luminance = (r * 0.299 + g * 0.587 + b * 0.114) / 255;
   // Determine the contrast color based on the luminance value
